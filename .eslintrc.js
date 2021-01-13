021fe1704c4fccc0d5f4578877563262581e7045
gucci-ninja <beautyspell5@gmail.com>
2021-01-13 12:16:41 -0500

    set up foreman, env variables and logo

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:vue/recommended"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+};
